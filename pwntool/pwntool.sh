f2b657aad577849b3ed2fff6428c8fbdf6da340e
minhlongmt183 <49335468+minhlongmt183@users.noreply.github.com>
2022-04-25 21:36:09 +0700

    Update pwntool.sh

--- pwntool/pwntool.sh
+++ pwntool/pwntool.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install python3 python3-pip python3-dev git libssl-dev libffi-dev build-essential
+sudo apt-get -y update
+sudo apt-get -y install python3 python3-pip python3-dev git libssl-dev libffi-dev build-essential
 python3 -m pip3 install --upgrade pip3
 python3 -m pip install --upgrade pwntools
